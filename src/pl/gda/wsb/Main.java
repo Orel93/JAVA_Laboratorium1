d5d4f81365463042257c75b667ad43cef12730e1
Dawid Reszka <dawid.raul@gmail.com>
2021-02-28 14:04:47 +0100

    3_commit

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -10,37 +10,37 @@ public class Main {
 
         if (human1.age < human2.age)
         {
-            System.out.println("human 2 is older ");
+            System.out.println(human2.firstName+" is older "+human2.age);
         }
         else if (human2.age > human2.age)
         {
-            System.out.println("human 1 is older ");
+            System.out.println(human1.firstName+" is older "+human2.age);
         }
         else
         {
             System.out.println("they have the same age");
         }
-        System.out.println(human1);
-        System.out.println(human1.firstName);
+        //System.out.println(human1);
+        //System.out.println(human1.firstName);
 
         Animal animal1 = new Animal ("monkey", 5, 24.50);
         Animal animal2 = new Animal ("dog",10,21.12);
 
         if (animal1.animalWeight < animal2.animalWeight)
         {
-            System.out.println("animal2 is heavier ");
+            System.out.println(animal2.animalName+" is heavier "+animal2.animalWeight);
         }
         else if (animal1.animalWeight > animal2.animalWeight)
         {
-            System.out.println("animal1 is heavier");
+            System.out.println(animal1.animalName+" is heavier "+animal1.animalWeight);
         }
         else
         {
-            System.out.println("they weigh the same");
+            System.out.println(animal1.animalName+"and"+animal2.animalName+" weigh the same");
         }
 
-        System.out.println(animal2);
-        System.out.println(animal2.animalAge);
+        //System.out.println(animal2);
+        //System.out.println(animal2.animalAge);
 
         Phone phone1 = new Phone ("Nokia", 123321521, "blue");
         Phone phone2 = new Phone ("Xiaomi", 523232123, "red");
