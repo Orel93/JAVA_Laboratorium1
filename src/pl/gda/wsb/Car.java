c068e15f86246c09d767e2d9701334d19e96aab4
Dawid Reszka <dawid.raul@gmail.com>
2021-03-28 12:36:03 +0200

    Zadanie 6

---
+++ src/pl/gda/wsb/Car.java
@@ -0,0 +1,17 @@
+package pl.gda.wsb;
+
+public class Car {
+
+    final String marka;
+    final String model;
+    String nrRejestracyjny ;
+    String kolor;
+
+
+    public Car(String marka, String model, String nrRejestracyjny, String kolor) {
+        this.marka = marka;
+        this.model = model;
+        this.nrRejestracyjny = nrRejestracyjny;
+        this.kolor = kolor;
+    }
+}
